c077bb7d34e02c881e480e1c444450b1f729b3dd
Scott Davis <stdavis@utah.gov>
2015-09-10 07:19:14 -0600

    handle if there are no matches

--- update.py
+++ update.py
@@ -13,27 +13,27 @@ def updateFGDBfromSDE(fgdb, sde, logger=None):
     fgdb: file geodatabase
     sde: sde geodatabase connection
     logger: agrc.logging.Logger (optional)
-    
+
     returns: String[] - the list of errors
-    
+
     Loops through the file geodatabase feature classes and looks for
     matches in the SDE database. If there is a match, it does a schema check
     and then updates the data.
     """
-    
+
     def log(msg):
         if logger:
             logger.logMsg(msg)
         else:
             print msg
-            
+
     def updateData():
         try:
             # validate that there was not a schema change
             arcpy.env.workspace = fgdb
             layer = sdeFC + '_Layer'
             arcpy.MakeFeatureLayer_management(sdeFC, layer, '1 = 2')
-            
+
             try:
                 arcpy.Append_management(layer, f, 'TEST')
                 log('schema test passed')
@@ -50,41 +50,42 @@ def updateFGDBfromSDE(fgdb, sde, logger=None):
                 else:
                     raise e
             arcpy.Delete_management(layer)
-            
+
             log('checking for changes...')
             if checkForChanges(f, sdeFC) and passed:
                 log('updating data...')
                 arcpy.TruncateTable_management(f)
-                
+
                 # edit session required for data that participates in relationships
                 editSession = arcpy.da.Editor(fgdb)
                 editSession.startEditing(False, False)
                 editSession.startOperation()
-                
+
                 fields = [fld.name for fld in arcpy.ListFields(f)]
                 fields = filter_fields(fields)
                 fields.append('SHAPE@')
                 with arcpy.da.InsertCursor(f, fields) as icursor, arcpy.da.SearchCursor(sdeFC, fields, sql_clause=(None, 'ORDER BY OBJECTID')) as cursor:
                     for row in cursor:
+                        print(row[0])
                         icursor.insertRow(row)
-                
+
                 editSession.stopOperation()
                 editSession.stopEditing(True)
-                
+
                 changes.append(f.upper())
             else:
                 log('no changes found')
-                
+
             return passed
         except:
             errors.append('Error updating: {}'.format(f))
             if logger:
                 logger.logError()
             return passed
-    
+
     log('** Updating {} from {}'.format(fgdb, sde))
     errors = []
-    
+
     # loop through local feature classes
     arcpy.env.workspace = fgdb
     fcs = arcpy.ListFeatureClasses()
@@ -93,13 +94,13 @@ def updateFGDBfromSDE(fgdb, sde, logger=None):
     for f in fcs:
         i = i + 1
         log('{} of {} | {}'.format(i, totalFcs, f))
-        
+
         found = False
-        
+
         # search for match in stand-alone feature classes
         arcpy.env.workspace = sde
         matches = arcpy.ListFeatureClasses('*.{}'.format(f))
-        if len(matches) > 0:
+        if matches is not None and len(matches) > 0:
             match = matches[0]
             sdeFC = join(sde, match)
             found = True
@@ -110,7 +111,7 @@ def updateFGDBfromSDE(fgdb, sde, logger=None):
                 # loop through datasets
                 for ds in datasets:
                     matches = arcpy.ListFeatureClasses('*.{}'.format(f), None, ds)
-                    if len(matches) > 0:
+                    if matches is not None and len(matches) > 0:
                         match = matches[0]
                         sdeFC = join(sde, match)
                         found = True
@@ -120,9 +121,9 @@ def updateFGDBfromSDE(fgdb, sde, logger=None):
             errors.append("{}: {}".format(f, msg))
             log(msg)
             continue
-        
+
         updateData()
-        
+
     return (errors, changes)
 
 def wasModifiedToday(fcname, fgdb):
@@ -141,31 +142,31 @@ def getFieldDifferences(ds1, ds2):
         returnFlds = []
         for f in flds:
             returnFlds.append(f.name)
-        
+
         returnFlds.sort()
         return returnFlds
-    
+
     ds1Flds = getFields(ds1)
     ds2Flds = getFields(ds2)
-    
+
     return "{} Fields: \n{}\n{} Fields: \n{}".format(ds1, ds1Flds, ds2, ds2Flds)
 
 def checkForChanges(f, sde):
     """
     returns False if there are no changes
-    
+
     """
     # try simple feature count first
     fCount = int(arcpy.GetCount_management(f).getOutput(0))
     sdeCount = int(arcpy.GetCount_management(sde).getOutput(0))
     if fCount != sdeCount:
         return True
-    
+
     fields = [fld.name for fld in arcpy.ListFields(f)]
-    
+
     # filter out shape fields
     fields = filter_fields(fields)
-    
+
     d = arcpy.Describe(f)
     shapeType = d.shapeType
     if shapeType == 'Polygon':
@@ -177,7 +178,7 @@ def checkForChanges(f, sde):
     else:
         shapeToken = 'SHAPE@JSON'
     fields.append(shapeToken)
-    
+
     def parseShape(shapeValue):
         if shapeValue is None:
             return 0
@@ -190,7 +191,7 @@ def checkForChanges(f, sde):
                 return 0
         else:
             return shapeValue
-        
+
     changed = False
     with arcpy.da.SearchCursor(f, fields, sql_clause=(None, 'ORDER BY OBJECTID')) as fCursor, arcpy.da.SearchCursor(sde, fields, sql_clause=(None, 'ORDER BY OBJECTID')) as sdeCursor:
         for fRow, sdeRow in izip(fCursor, sdeCursor):
@@ -210,7 +211,7 @@ def checkForChanges(f, sde):
                     except AssertionError:
                         changed = True
                         break
-                
+
                 # trim microseconds since they can be off by one between file and sde databases
                 for i in range(len(fRow)):
                     if type(fRow[i]) is datetime:
@@ -218,10 +219,10 @@ def checkForChanges(f, sde):
                         sdeRow = list(sdeRow)
                         fRow[i] = fRow[i].replace(microsecond=0)
                         sdeRow[i] = sdeRow[i].replace(microsecond=0)
-                
+
                 # compare all values except OBJECTID
                 if fRow[1:] != sdeRow[1:]:
                     changed = True
                     break
-    
+
     return changed
