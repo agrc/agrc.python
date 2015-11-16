a9f5eccb10be8d11880f09f966fc5bcfe224d0cd
Scott Davis <stdavis@utah.gov>
2015-11-16 16:21:19 -0700

    prep for publish to pypi

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='agrc',
+    version='1.0.0',
+    author='Scott Davis',
+    author_email='Scott Davis',
+    packages=['agrc', 'agrc.test'],
+    url='https://github.com/agrc/agrc.python',
+    license='LICENSE.txt',
+    description='A collection of python modules that make life easier for us at AGRC.',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        'requests >= 2.8.1'
+    ]
+)
