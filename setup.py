c34600472833355d82881e11ec2db291d93f05a0
Scott Davis <stdavis@utah.gov>
2015-11-17 16:44:48 -0700

    v1.0.1 version bump and changelog

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='agrc',
-    version='1.0.0',
+    version='1.0.1',
     author='Scott Davis',
     author_email='Scott Davis',
     packages=['agrc', 'agrc.test'],
